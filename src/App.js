7c043fec8fd948172f97501b0bd5f59cb5ea47ec
Akhmetali <44460302+Akhmetali@users.noreply.github.com>
2022-09-07 12:05:12 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { Row, Col } from 'antd';
+import { BsTelephoneFill, BsCheckLg } from "react-icons/bs"
+import { FaBars } from "react-icons/fa"
+import ProductBox from './components/productBox';
+function App() {
+  return (
+    <div className="App">
+      <div className='App-header-tag'>
+        <Row gutter={[16,16]} justify="end">
+          <Col className='contact1'>
+            <p><BsTelephoneFill/>(+976) 736-8151</p>
+          </Col>
+          <Col className='contact2'>
+            <p className='alba'>Борлуулалтын алба</p>
+          </Col>
+          <Col className='contact3'>
+            <p><BsTelephoneFill/>(+976) 7227-8888</p>
+          </Col>
+        </Row>
+      </div>
+      <div className='App-header'>
+          <div className='menus'>
+            <div className='icon'>
+              <img src='https://i.postimg.cc/KjvTk1yn/Logo2022.png'/>
+              <div className='bar'>
+                  <FaBars/>
+              </div>
+              
+            </div>
+            <div className='menu'>
+                <p>Бидний тухай</p>
+                <p>Хүний нөөц</p>
+                <p>Авлигын эсрэг үйл ажиллагаа</p>
+                <p>Үйлдвэрчний эвлэлийн хороо</p>
+                <p>Үйлдвэрчний эвлэлийн хороо</p>
+                <button>Захиалга</button>
+                <button> Ил тод байдал</button>
+            </div>
+            
+          </div>
+      </div>
+      <div className='section'>
+        <h1 className='section-title'>Чанартай бүтээгдэхүүнийг хамгийн хямдаар</h1>
+        <div className='section-btns'>
+            <button>Цемент</button>
+            <button>Шохой</button>
+        </div>
+        <div className='section-second'>
+          <div className="productBox">
+              <h3>ЦЕМЕНТ 42.5</h3>
+              <div className='price'>
+                  <div className='price-tog'>
+                      <p className='tog'>202,000₮</p>
+                      <div className='noat'>
+                          <p>НӨАТ-тай</p>
+                          <p>Жижиглэн (байхгүй)</p>
+                      </div>
+                  </div>
+                  <div className='btns'>
+                    <button>Цемент 32.5 захиалах</button>
+                  </div>
+              </div>
+              <div className='detail'>
+                <p><BsCheckLg/>Задгай /бөөний үнэ/ - 202,000₮</p>
+                <p><BsCheckLg/>Ууттай /бөөний үнэ/ - 227,000₮</p>
+              </div>
+          </div>
+          
+        </div>
+      </div>
+
+
+     
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/productBox.js
@@ -0,0 +1,16 @@
+
+const ProductBox=()=>{
+    <div className="productBox">
+        <h1>ЦЕМЕНТ 42.5</h1>
+        <div>
+            <p>202,000₮</p>
+            <div>
+                <p>НӨАТ-тай</p>
+                <p>Жижиглэн (220,000₮)</p>
+            </div>
+        </div>
+
+    </div>
+}
+
+export default ProductBox;
